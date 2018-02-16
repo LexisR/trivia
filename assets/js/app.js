1e6bc59d95509e5f4b47775d1cdfd4219a1c86bb
lexisR <lexisrodriguez22@gmail.com>
2018-02-16 11:21:02 -0300

    funcion que contiene las 4 respuesta y las muestra

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,9 +2,31 @@ fetch('https://opentdb.com/api.php?amount=10&category=11&difficulty=medium')
 .then((response)=>{
     console.log(response);
     return response.json();
-  })
-
-  .then((data)=>{
-     console.log(data)
-  
+})
+.then((data)=>{
+  console.log(data)
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//funcion para sacar las respuestas incorrectas
+let answersArray = [];
+function parseAnswers(incorrect_answers) {
+  for (let i = 0; i < incorrect_answers.length; i++) {
+    returnArray.push(incorrect_answers[i]);
+  }
+  return console.log(answersArray);
+}
+
